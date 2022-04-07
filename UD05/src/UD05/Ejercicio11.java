48ce08465913f9a83d3be9112f08df9e80ce00a4
Gerard Viñuela <gerardviru@gmail.com>
2022-04-07 09:59:48 +0200

    Proyecto UD05-FlujoDatos

--- UD05/src/UD05/Ejercicio11.java
+++ UD05/src/UD05/Ejercicio11.java
@@ -1,10 +1,41 @@
 package UD05;
 
+import java.util.Scanner;
+
 public class Ejercicio11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce un dia de la semana: ");
+		String diaS = sc.next();
+		sc.close();
+		
+		switch(diaS) {
+		case "Lunes":
+			System.out.println(diaS + " es dia Laboral");
+			break;
+		case "Martes":
+			System.out.println(diaS + " es dia Laboral");
+			break;
+		case "Miercoles":
+			System.out.println(diaS + " es dia Laboral");
+			break;	
+		case "Jueves":
+			System.out.println(diaS + " es dia Laboral");
+			break;
+		case "Viernes":
+			System.out.println(diaS + " es dia Laboral");
+			break;
+		case "Sábado":
+			System.out.println(diaS + " no es dia Laboral");
+			break;
+		case "Domingo":
+			System.out.println(diaS + " no es dia Laboral");
+			break;
+		default:
+			System.out.println("No has introducido el dia correctamente, vuelva a intentarlo: ");
+		}
 	}
-
 }
---
+++ UD05/src/UD05/Ejercicio12.java
@@ -0,0 +1,10 @@
+package UD05;
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
