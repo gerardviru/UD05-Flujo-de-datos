7392ec799f2dfe1e37ac49989eea825d07bc1719
Gerard Viñuela <gerardviru@gmail.com>
2022-04-07 10:02:24 +0200

    Proyecto UD05-FlujoDatos

--- UD05/src/UD05/Ejercicio11.java
+++ UD05/src/UD05/Ejercicio11.java
@@ -6,12 +6,15 @@ public class Ejercicio11 {
 
 	public static void main(String[] args) {
 		
+		/*Instanciamos un imput de datos del cliente*/
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Introduce un dia de la semana: ");
+		/*Imput por teclado del dia de la semana*/
 		String diaS = sc.next();
 		sc.close();
 		
+		/*Instanciamos un switch que nos permite definir el Output que realizara en cada caso posible*/
 		switch(diaS) {
 		case "Lunes":
 			System.out.println(diaS + " es dia Laboral");
@@ -34,6 +37,7 @@ public class Ejercicio11 {
 		case "Domingo":
 			System.out.println(diaS + " no es dia Laboral");
 			break;
+			/*Controlamos los errores de introduccion de datos que puedan existir*/
 		default:
 			System.out.println("No has introducido el dia correctamente, vuelva a intentarlo: ");
 		}
